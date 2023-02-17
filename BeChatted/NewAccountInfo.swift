58c7b334836158295a23a1af59bfe585d559e0c4
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-02-17 20:36:21 +0200

    Add NewAccountInfo model

---
+++ BeChatted/NewAccountInfo.swift
@@ -0,0 +1,13 @@
+//
+//  NewAccountInfo.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 17.02.2023.
+//
+
+import Foundation
+
+struct NewAccountInfo {
+    let email: String
+    let password: String
+}
