e6200901a68cacdfcf8275988a0f6d557b85b1f8
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-03-30 20:06:50 +0300

    Add NewUserInfo model

---
+++ BeChatted/Add New User Feature/NewUserInfo.swift
@@ -0,0 +1,22 @@
+//
+//  NewUserInfo.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 30.03.2023.
+//
+
+import Foundation
+
+public struct NewUserInfo: Codable, Equatable {
+    public let name: String
+    public let email: String
+    public let avatarName: String
+    public let avatarColor: String
+    
+    public init(name: String, email: String, avatarName: String, avatarColor: String) {
+        self.name = name
+        self.email = email
+        self.avatarName = avatarName
+        self.avatarColor = avatarColor
+    }
+}
