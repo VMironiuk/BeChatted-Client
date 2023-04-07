bcf9adfdda2733c7a21779862075ea3ad5ef872e
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-04-07 19:36:10 +0300

    Add makeSUT() and anyURL() factory helper methods

--- BeChattedTests/UserLogoutServiceTests.swift
+++ BeChattedTests/UserLogoutServiceTests.swift
@@ -29,7 +29,7 @@ final class UserLogoutServiceTests: XCTestCase {
     func test_init_doesNotSendRequestByURL() {
         // given
         let client = HTTPClientSpy()
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         
         // when
         _ = UserLogoutService(url: url, client: client)
@@ -40,9 +40,8 @@ final class UserLogoutServiceTests: XCTestCase {
     
     func test_send_sendsLogoutRequestByURL() {
         // given
-        let client = HTTPClientSpy()
-        let url = URL(string: "http://any-url.com")!
-        let sut = UserLogoutService(url: url, client: client)
+        let url = anyURL()
+        let (sut, client) = makeSUT(url: url)
         
         // when
         sut.send()
@@ -53,9 +52,8 @@ final class UserLogoutServiceTests: XCTestCase {
 
     func test_send_sendsLogoutRequestByURLTwice() {
         // given
-        let client = HTTPClientSpy()
-        let url = URL(string: "http://any-url.com")!
-        let sut = UserLogoutService(url: url, client: client)
+        let url = anyURL()
+        let (sut, client) = makeSUT(url: url)
         
         // when
         sut.send()
@@ -70,6 +68,24 @@ final class UserLogoutServiceTests: XCTestCase {
     // 6. send() delivers successful result on any HTTP response
     
     // MARK: - Helpers
+    
+    private func makeSUT(
+        url: URL = URL(string: "http://any-url.com")!,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (UserLogoutService, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = UserLogoutService(url: url, client: client)
+        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, client)
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
     
     private class HTTPClientSpy: HTTPClientProtocol {
         private(set) var requestedURLs = [URL]()
