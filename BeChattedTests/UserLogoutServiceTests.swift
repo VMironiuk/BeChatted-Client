20b68c15cff55ca842584476193c9959869a6cbe
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-04-07 19:18:32 +0300

    UserLogoutService.init() doesn't send request by URL

--- BeChattedTests/UserLogoutServiceTests.swift
+++ BeChattedTests/UserLogoutServiceTests.swift
@@ -6,13 +6,41 @@
 //
 
 import XCTest
+import BeChatted
+
+final class UserLogoutService {
+    private let url: URL
+    private let client: HTTPClientProtocol
+    
+    init(url: URL, client: HTTPClientProtocol) {
+        self.url = url
+        self.client = client
+    }
+}
 
 final class UserLogoutServiceTests: XCTestCase {
     
-    // 1. init() does not send a request by URL
+    func test_init_doesNotSendRequestByURL() {
+        let client = HTTPClientSpy()
+        let url = URL(string: "http://any-url.com")!
+        _ = UserLogoutService(url: url, client: client)
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
     // 2. send() sends logout request by URL
     // 3. send() sends logout request by URL twice
     // 4. send() delivers connectivity error on client error
     // 5. send() does not deliver result after the instance has been deallocated
     // 6. send() delivers successful result on any HTTP response
+    
+    // MARK: - Helpers
+    
+    private class HTTPClientSpy: HTTPClientProtocol {
+        private(set) var requestedURL: URL?
+        
+        func perform(request: URLRequest, completion: @escaping (BeChatted.HTTPClientResult) -> Void) {
+            requestedURL = request.url
+        }
+    }
 }
