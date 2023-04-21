28021338401a273546ead660cf0d0bb49a22e827
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-04-21 17:29:12 +0300

    Add anyURL() helper factory method

--- BeChattedTests/URLSessionHTTPClientTests.swift
+++ BeChattedTests/URLSessionHTTPClientTests.swift
@@ -37,7 +37,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_performRequest_performsCorrectRequest() {
-        let expectedRequest = URLRequest(url: URL(string: "https://any-url.com")!)
+        let expectedRequest = URLRequest(url: anyURL())
         
         let exp = expectation(description: "Wait for request")
         URLProtocolStub.observeRequests { receivedRequest in
@@ -50,7 +50,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_performRequest_failsOnRequestError() {
-        let request = URLRequest(url: URL(string: "http://any-url.com")!)
+        let request = URLRequest(url: anyURL())
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
                 
@@ -75,6 +75,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
