97c7aed18a6fcbe0a38fcd4abe0181945a03e6a9
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-04-25 19:47:36 +0300

    Move URLSessionHTTPClient to its own file in production

--- BeChattedTests/URLSessionHTTPClientTests.swift
+++ BeChattedTests/URLSessionHTTPClientTests.swift
@@ -8,28 +8,6 @@
 import XCTest
 import BeChatted
 
-final class URLSessionHTTPClient: HTTPClientProtocol {
-    struct UnexpectedValuesRepresentation: Error {}
-    
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    func perform(request: URLRequest, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {
