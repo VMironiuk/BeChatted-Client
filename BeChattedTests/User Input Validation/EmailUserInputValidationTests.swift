c62bbfabaec432bf1502ecce82ceb8d5730c9019
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-05-23 19:41:08 +0300

    EmailValidator.isValid() returns false for missing 'at' symbol

---
+++ BeChattedTests/User Input Validation/EmailUserInputValidationTests.swift
@@ -0,0 +1,27 @@
+//
+//  EmailUserInputValidationTests.swift
+//  BeChattedTests
+//
+//  Created by Volodymyr Myroniuk on 23.05.2023.
+//
+
+import XCTest
+
+struct EmailValidator {
+    func isValid(_ email: String) -> Bool {
+        false
+    }
+}
+
+final class EmailUserInputValidationTests: XCTestCase {
+
+    func test_isValid_returnsFalseForMissingAtSymbol() {
+        // given
+        let email = "emailexample.com"
+        let sut = EmailValidator()
+        
+        // when
+        // then
+        XCTAssertFalse(sut.isValid(email))
+    }
+}
