08811cc9baeaecec65cdf0050f674460647a7d92
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-05-23 19:42:39 +0300

    EmailValidator/isValid() returns false for two 'at' symbols

--- BeChattedTests/User Input Validation/EmailUserInputValidationTests.swift
+++ BeChattedTests/User Input Validation/EmailUserInputValidationTests.swift
@@ -24,4 +24,14 @@ final class EmailUserInputValidationTests: XCTestCase {
         // then
         XCTAssertFalse(sut.isValid(email))
     }
+    
+    func test_isValid_returnsFalseForTwoAtSymbols() {
+        // given
+        let email = "email@@example.com"
+        let sut = EmailValidator()
+        
+        // when
+        // then
+        XCTAssertFalse(sut.isValid(email))
+    }
 }
