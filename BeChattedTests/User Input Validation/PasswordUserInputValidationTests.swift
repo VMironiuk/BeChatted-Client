2acdb0632df85b4bce95eba92061bb65a256089e
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-06-02 19:12:06 +0300

    PasswordValidator.isValid() returns false is password in less than 8 characters long

---
+++ BeChattedTests/User Input Validation/PasswordUserInputValidationTests.swift
@@ -0,0 +1,37 @@
+//
+//  PasswordUserInputValidationTests.swift
+//  BeChattedTests
+//
+//  Created by Volodymyr Myroniuk on 02.06.2023.
+//
+
+import XCTest
+
+struct PasswordValidator {
+    func isValid(_ password: String) -> Bool {
+        false
+    }
+}
+
+final class PasswordUserInputValidationTests: XCTestCase {
+    
+    func test_isValid_returnsFalseIfPasswordIsLessThan8CharactersLong() {
+        // given
+        let password = "1234"
+        let sut = PasswordValidator()
+        
+        // when
+        // then
+        XCTAssertFalse(sut.isValid(password))
+    }
+    
+    func test_isValid_returnsFalseIfPasswordIsEmpty() {
+        // given
+        let password = ""
+        let sut = PasswordValidator()
+        
+        // when
+        // then
+        XCTAssertFalse(sut.isValid(password))
+    }
+}
