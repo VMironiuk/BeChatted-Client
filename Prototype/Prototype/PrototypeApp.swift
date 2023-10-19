75d50de0a74b44fbf7152fd88a248aafc7e898e5
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-10-19 18:07:42 +0300

    Add initial iOS prototype

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Volodymyr Myroniuk on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
