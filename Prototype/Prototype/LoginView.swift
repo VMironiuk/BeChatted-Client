24db3388a9966a6dd0433e8a23b952fc8bf858fe
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-10-24 18:57:20 +0300

    Extract header from login view to reusable auth header view

--- Prototype/Prototype/LoginView.swift
+++ Prototype/Prototype/LoginView.swift
@@ -10,24 +10,11 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         VStack {
-            HStack {
-                VStack(alignment: .leading, spacing: 20) {
-                    Spacer()
-                    
-                    Text("Sign in to your\nAccount")
-                        .font(.system(size: 24, weight: .semibold))
-                        .foregroundStyle(Color("Auth/Header/TitleColor"))
-                    Text("Sign in to your Account")
-                        .font(.system(size: 12, weight: .regular))
-                        .foregroundStyle(Color("Auth/Header/SubtitleColor"))
-                }
-                .padding(.bottom, 32)
-                
-                Spacer()
-            }
-            .padding(.horizontal, 20)
+            AuthHeaderView(
+                title: "Sign in to your\nAccount",
+                subtitle: "Sign in to your Account"
+            )
             .frame(height: 220)
-            .background(Color("Auth/Header/HeaderColor"))
             
             Spacer()
         }
