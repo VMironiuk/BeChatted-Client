7b77231dd94f68ec334fad847dece190f8dbb7e4
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-10-26 19:14:43 +0300

    Refactor text input view

--- Prototype/Prototype/TextInputView.swift
+++ Prototype/Prototype/TextInputView.swift
@@ -11,25 +11,26 @@ struct TextInputView: View {
     let title: String
     
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            TextField("", text: .constant("example@mail.com"))
-                .padding(.horizontal, 20)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color("Auth/UserInput/BorderColor"), lineWidth: 1)
-                        .frame(height: 50)
-                )
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color("Auth/UserInput/BorderColor"), lineWidth: 1)
             
-            Text(title)
-                .font(.system(size: 12, weight: .medium))
-                .foregroundStyle(Color(("Auth/UserInput/TitleColor")))
-                .padding(.horizontal, 8)
-                .background(Color.white)
-                .offset(x: 4, y: -20)
+            GeometryReader { geometry in
+                Text(title)
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundStyle(Color(("Auth/UserInput/TitleColor")))
+                    .padding(.horizontal, 8)
+                    .background(Color.white)
+                    .offset(x: 4, y: -8)
+            }
+            
+            TextField("", text: .constant("mail@example.com"))
+                .padding(.horizontal, 20)
         }
     }
 }
 
 #Preview {
     TextInputView(title: "Email")
+        .frame(height: 60)
 }
