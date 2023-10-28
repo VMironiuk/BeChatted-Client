c1c23fe7093bf865e3fc8fa37f5fad5053eb3fee
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-10-28 14:11:10 +0300

    Add the register view

---
+++ Prototype/Prototype/RegisterView.swift
@@ -0,0 +1,78 @@
+//
+//  RegisterView.swift
+//  Prototype
+//
+//  Created by Volodymyr Myroniuk on 28.10.2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var isActive = true
+    
+    var body: some View {
+        VStack {
+            AuthHeaderView(
+                title: "Register",
+                subtitle: "Create your Account"
+            )
+            .frame(height: 220)
+            
+            TextInputView(title: "Your Name")
+                .frame(height: 50)
+                .padding(.horizontal, 20)
+                .padding(.top, 64)
+            
+            TextInputView(title: "Email")
+                .frame(height: 50)
+                .padding(.horizontal, 20)
+                .padding(.top, 16)
+            
+            SecureInputView(title: "Password")
+                .frame(height: 50)
+                .padding(.horizontal, 20)
+                .padding(.top, 16)
+            
+            Spacer()
+            
+            Button("Register") {
+                
+            }
+            .buttonStyle(MainButtonStyle(isActive: isActive))
+            .padding(.horizontal, 20)
+            .padding(.bottom, 32)
+            
+            HStack {
+                Text("Already have an account?")
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundStyle(Color("Auth/BottomLabelColor"))
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundStyle(Color("Auth/MainButtonColor"))
+                }
+            }
+            .padding(.bottom, 40)
+        }
+        .ignoresSafeArea(.keyboard)
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil,
+            from: nil,
+            for: nil
+        )
+    }
+}
+
+#Preview {
+    RegisterView()
+}
