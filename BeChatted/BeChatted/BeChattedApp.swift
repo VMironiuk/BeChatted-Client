d401d0091f3e581204b99c60b6b901bc8941743e
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-11-04 06:36:06 +0200

    Add BeChatted iOS app project

---
+++ BeChatted/BeChatted/BeChattedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeChattedApp.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeChattedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
