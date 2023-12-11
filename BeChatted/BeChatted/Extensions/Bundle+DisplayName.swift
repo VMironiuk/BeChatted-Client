437489b2a2d3035b2684016b91dd6e3bea1df079
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2023-12-11 15:39:11 +0200

    Get the app name from the bundle

---
+++ BeChatted/BeChatted/Extensions/Bundle+DisplayName.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle+DisplayName.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 11.12.2023.
+//
+
+import Foundation
+
+extension Bundle {
+    var displayName: String? {
+        object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
+    }
+    
+    var displayNameOrEmpty: String {
+        displayName ?? ""
+    }
+}
--- BeChatted/BeChatted/Modules/Auth/Register/RegistrationSuccessMessage.swift
+++ BeChatted/BeChatted/Modules/Auth/Register/RegistrationSuccessMessage.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 struct RegistrationSuccessMessage: MessageProtocol {
-    // TODO: fetch the app's name from the bundle
-    let title = "Welcome to BeChatted!"
+    static private var appName: String {
+        Bundle.main.displayNameOrEmpty
+    }
+    let title = "Welcome to \(appName)!"
     let description = "Congratulations, your registration is complete! "
         + "To begin connecting, chatting, and sharing moments with friends and "
-        + "family, please log in to your new BeChatted account. Discover the "
-        + "world of BeChatted and make every conversation count."
+        + "family, please log in to your new \(appName) account. Discover the "
+        + "world of \(appName) and make every conversation count."
 }
