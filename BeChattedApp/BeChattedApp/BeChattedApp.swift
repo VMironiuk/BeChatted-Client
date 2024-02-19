a3fb4a3ed710248fd97d945047daf83c8fba433d
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-19 08:12:23 +0200

    Add a new BeChattedApp project

---
+++ BeChattedApp/BeChattedApp/BeChattedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeChattedApp.swift
+//  BeChattedApp
+//
+//  Created by Volodymyr Myroniuk on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BeChattedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
