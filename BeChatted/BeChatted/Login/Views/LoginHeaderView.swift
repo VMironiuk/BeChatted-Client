2e18c82986bd2915f52f135e9a5f027c1b4c39ba
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-23 12:33:58 +0200

    Uncomment LoginHeaderView

--- BeChatted/BeChatted/Login/Views/LoginHeaderView.swift
+++ BeChatted/BeChatted/Login/Views/LoginHeaderView.swift
@@ -5,25 +5,25 @@
 //  Created by Volodymyr Myroniuk on 02.02.2024.
 //
 
-//import SwiftUI
-//
-//struct LoginHeaderView: View {
-//    @Environment(\.isKeyboardShown) var isKeyboardShown
-//    
-//    var body: some View {
-//        VStack {
-//            if !isKeyboardShown {
-//                AuthHeaderView(
-//                    title: "Sign in to your\nAccount",
-//                    subtitle: "Sign in to your Account"
-//                )
-//                .frame(height: 180)
-//                .transition(.offset(y: -260))
-//            }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    LoginHeaderView()
-//}
+import SwiftUI
+
+struct LoginHeaderView: View {
+    @Environment(\.isKeyboardShown) var isKeyboardShown
+    
+    var body: some View {
+        VStack {
+            if !isKeyboardShown {
+                AuthHeaderView(
+                    title: "Sign in to your\nAccount",
+                    subtitle: "Sign in to your Account"
+                )
+                .frame(height: 180)
+                .transition(.offset(y: -260))
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginHeaderView()
+}
