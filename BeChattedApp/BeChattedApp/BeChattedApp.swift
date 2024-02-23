4900d38f3c7acc1f74361a1e9e78a2cee3715d73
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-23 15:09:29 +0200

    Add possibility to hide keyboard

--- BeChattedApp/BeChattedApp/BeChattedApp.swift
+++ BeChattedApp/BeChattedApp/BeChattedApp.swift
@@ -13,39 +13,9 @@ import SwiftUI
 
 @main
 struct BeChattedApp: App {
-    let loginViewModel = BeChattediOS.LoginViewModel(
-        emailValidator: EmailValidator(),
-        passwordValidator: PasswordValidator(),
-        authService: AuthServiceComposer.authService)
-    
-    var loginView: some View {
-        LoginViewComposer.composedLoginView(
-            with: loginViewModel,
-            onTapped: { print("ON TAPPED") },
-            onLoginButtonTapped: { print("ON TAPPED LOGIN BUTTON") },
-            onRegisterButtonTapped: { print("ON TAPPED REGISTER BUTTON") },
-            onLoginSuccessAction: { print("ON LOGIN SUCCES ACTION") })
-    }
-    
     var body: some Scene {
         WindowGroup {
-            loginView
+            LoginFeatureComposer.loginView
         }
-    }
-}
-
-extension EmailValidator: BeChattediOS.EmailValidatorProtocol {}
-extension PasswordValidator: BeChattediOS.PasswordValidatorProtocol {}
-extension AuthService: BeChattediOS.AuthServiceProtocol {
-    public func addUser(_ payload: BeChattediOS.AddUserPayload, authToken: String, completion: @escaping (Result<BeChattediOS.AddedUserInfo, BeChattediOS.AuthError>) -> Void) {
-        
-    }
-    
-    public func login(_ payload: BeChattediOS.LoginPayload, completion: @escaping (Result<BeChattediOS.LoginInfo, BeChattediOS.AuthError>) -> Void) {
-        
-    }
-    
-    public func createAccount(_ payload: BeChattediOS.CreateAccountPayload, completion: @escaping (Result<Void, BeChattediOS.AuthError>) -> Void) {
-        
     }
 }
---
+++ BeChattedApp/BeChattedApp/LoginFeatureComposer.swift
@@ -0,0 +1,55 @@
+//
+//  LoginFeatureComposer.swift
+//  BeChattedApp
+//
+//  Created by Volodymyr Myroniuk on 23.02.2024.
+//
+
+import BeChattedAuth
+import BeChattediOS
+import BeChattedUserInputValidation
+import SwiftUI
+
+struct LoginFeatureComposer {
+    private init() {}
+        
+    static var loginView: some View {
+        let loginViewModel = BeChattediOS.LoginViewModel(
+            emailValidator: EmailValidator(),
+            passwordValidator: PasswordValidator(),
+            authService: AuthServiceComposer.authService)
+
+        return LoginViewComposer.composedLoginView(
+            with: loginViewModel,
+            onTapped: { UIApplication.shared.hideKeyboard() },
+            onLoginButtonTapped: { print("ON TAPPED LOGIN BUTTON") },
+            onRegisterButtonTapped: { print("ON TAPPED REGISTER BUTTON") },
+            onLoginSuccessAction: { print("ON LOGIN SUCCES ACTION") })
+    }
+}
+
+
+extension EmailValidator: BeChattediOS.EmailValidatorProtocol {}
+
+extension PasswordValidator: BeChattediOS.PasswordValidatorProtocol {}
+
+extension AuthService: BeChattediOS.AuthServiceProtocol {
+    public func addUser(
+        _ payload: BeChattediOS.AddUserPayload,
+        authToken: String,
+        completion: @escaping (Result<BeChattediOS.AddedUserInfo, BeChattediOS.AuthError>) -> Void
+    ) {
+    }
+    
+    public func login(
+        _ payload: BeChattediOS.LoginPayload,
+        completion: @escaping (Result<BeChattediOS.LoginInfo, BeChattediOS.AuthError>) -> Void
+    ) {
+    }
+    
+    public func createAccount(
+        _ payload: BeChattediOS.CreateAccountPayload,
+        completion: @escaping (Result<Void, BeChattediOS.AuthError>) -> Void
+    ) {
+    }
+}
