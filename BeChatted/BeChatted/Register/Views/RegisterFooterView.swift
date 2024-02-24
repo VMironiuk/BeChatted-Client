95b89b6bc3ae30c45232224fe593aa0106f20a77
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-24 11:50:27 +0200

    Uncomment RegisterFooterView

--- BeChatted/BeChatted/Register/Views/RegisterFooterView.swift
+++ BeChatted/BeChatted/Register/Views/RegisterFooterView.swift
@@ -5,29 +5,29 @@
 //  Created by Volodymyr Myroniuk on 02.02.2024.
 //
 
-//import SwiftUI
-//
-//struct RegisterFooterView: View {
-//    @EnvironmentObject var mainNavigationController: MainNavigationController
-//    
-//    var body: some View {
-//        HStack {
-//            Text("Already have an account?")
-//                .font(.system(size: 14, weight: .regular))
-//                .foregroundStyle(Color("Auth/BottomLabelColor"))
-//            
-//            Button {
-//                mainNavigationController.pop()
-//            } label: {
-//                Text("Login")
-//                    .font(.system(size: 14, weight: .regular))
-//                    .foregroundStyle(Color("Auth/MainButtonColor"))
-//            }
-//        }
-//        .padding(.bottom, 40)
-//    }
-//}
-//
-//#Preview {
-//    RegisterFooterView()
-//}
+import SwiftUI
+
+struct RegisterFooterView: View {
+    let onLoginButtonTapped: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text("Already have an account?")
+                .font(.system(size: 14, weight: .regular))
+                .foregroundStyle(Color("Auth/BottomLabelColor"))
+            
+            Button {
+                onLoginButtonTapped()
+            } label: {
+                Text("Login")
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundStyle(Color("Auth/MainButtonColor"))
+            }
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+#Preview {
+    RegisterFooterView {}
+}
