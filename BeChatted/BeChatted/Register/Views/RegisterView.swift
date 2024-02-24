263e575cf78b13f57c01360adf1b50062546fa6c
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-24 12:32:22 +0200

    Move RegisterViewComposer to a separate file

--- BeChatted/BeChatted/Register/Views/RegisterView.swift
+++ BeChatted/BeChatted/Register/Views/RegisterView.swift
@@ -7,33 +7,6 @@
 
 import SwiftUI
 
-public struct RegisterViewComposer {
-    private init() {}
-    
-    public static func composedRegisterView(
-        with viewModel: RegisterViewModel,
-        onViewTapped: @escaping () -> Void,
-        onRegisterButtonTapped: @escaping () -> Void,
-        onRegisterSuccessAction: @escaping () -> Void,
-        onBackButtonTapped: @escaping () -> Void,
-        onLoginButtonTapped: @escaping () -> Void
-    ) -> some View {
-        let headerView = RegisterHeaderView(onBackButtonTapped: onBackButtonTapped)
-        let footerView = AuthFooterView(
-            text: "Already have an account?",
-            buttonText: "Log in",
-            onButtonTapped: onLoginButtonTapped)
-        
-        var view = RegisterView(viewModel: viewModel, headerView: headerView, footerView: footerView)
-        
-        view.onTapped = onViewTapped
-        view.onRegisterButtonTapped = onRegisterButtonTapped
-        view.onRegisterSuccessAction = onRegisterSuccessAction
-        
-        return view
-    }
-}
-
 struct RegisterView: View {
     @Bindable private var viewModel: RegisterViewModel
     private let headerView: RegisterHeaderView
