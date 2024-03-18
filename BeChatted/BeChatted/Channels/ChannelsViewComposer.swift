ed81d0202a8181e5c5355807a41cf056f7343b05
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-18 09:32:59 +0200

    Add ChannelsViewComposer

---
+++ BeChatted/BeChatted/Channels/ChannelsViewComposer.swift
@@ -0,0 +1,16 @@
+//
+//  ChannelsViewComposer.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 18.03.2024.
+//
+
+import SwiftUI
+
+public struct ChannelsViewComposer {
+    private init() {}
+    
+    public static func channelsViewComposed() -> some View {
+        ChannelsView()
+    }
+}
