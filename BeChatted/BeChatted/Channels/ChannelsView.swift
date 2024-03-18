8bb13e368e3b396d3ad4c3e8dfbc4eaa3b1e9c4d
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-18 09:26:00 +0200

    Add ChannelsView starter file

---
+++ BeChatted/BeChatted/Channels/ChannelsView.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelsView.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 18.03.2024.
+//
+
+import SwiftUI
+
+struct ChannelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChannelsView()
+}
