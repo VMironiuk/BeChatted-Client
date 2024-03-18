641810871b1052c5b6ba26a3750fb9183690b3d6
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-03-18 09:52:45 +0200

    Add the navigation title

--- BeChatted/BeChatted/Channels/Views/ChannelsView.swift
+++ BeChatted/BeChatted/Channels/Views/ChannelsView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ChannelsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+        }
+        .navigationTitle("Channels")
     }
 }
 
 #Preview {
-    ChannelsView()
+    NavigationStack {
+        ChannelsView()
+    }
 }
