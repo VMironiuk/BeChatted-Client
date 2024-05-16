58fd3bf5a394965a4862447fc0f3bc69e805b4e2
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-05-16 08:58:44 +0300

    Shorter enum cases writing

--- BeChattedChannels/BeChattedChannels/ChannelsLoadingResultMapper.swift
+++ BeChattedChannels/BeChattedChannels/ChannelsLoadingResultMapper.swift
@@ -9,19 +9,19 @@ import Foundation
 
 struct ChannelsLoadingResultMapper {
     static func result(for data: Data?, response: HTTPURLResponse?) -> Result<[ChannelInfo], ChannelsLoadingError> {
-        guard let response = response else { return .failure(ChannelsLoadingError.unknown) }
+        guard let response = response else { return .failure(.unknown) }
         
         switch response.statusCode {
         case 200:
             return result(for: data)
         default:
-            return .failure(ChannelsLoadingError.server)
+            return .failure(.server)
         }
     }
     
     private static func result(for data: Data?) -> Result<[ChannelInfo], ChannelsLoadingError> {
         guard let data = data, let channels = try? JSONDecoder().decode([ChannelInfo].self, from: data) else {
-            return .failure(ChannelsLoadingError.invalidData)
+            return .failure(.invalidData)
         }
         
         return .success(channels)
