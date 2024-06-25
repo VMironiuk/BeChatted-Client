e7342224888c5b31c5935b2dbe6298760752989e
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-06-25 08:34:40 +0300

    LogoutViewModel.logout sends logout request

--- BeChatted/BeChattedTests/LogoutViewModelTests.swift
+++ BeChatted/BeChattedTests/LogoutViewModelTests.swift
@@ -10,9 +10,16 @@ import BeChatted
 
 public final class LogoutViewModel: ObservableObject {
   private let service: AuthServiceProtocol
+  private let authToken: String
   
-  public init(service: AuthServiceProtocol) {
+  public init(service: AuthServiceProtocol, authToken: String) {
     self.service = service
+    self.authToken = authToken
+  }
+  
+  public func logout() {
+    service.logout(authToken: authToken) { _ in
+    }
   }
 }
 
@@ -23,11 +30,23 @@ final class LogoutViewModelTests: XCTestCase {
     XCTAssertEqual(service.logoutCallCount, 0)
   }
   
+  func test_logout_sendsLogoutRequest() {
+    let (sut, service) = makeSUT()
+    
+    sut.logout()
+    
+    XCTAssertEqual(service.logoutCallCount, 1)
+  }
+  
   // MARK: - Helpers
   
-  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (LogoutViewModel, AuthServiceStub) {
+  private func makeSUT(
+    authToken: String = "any-auth-token",
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) -> (LogoutViewModel, AuthServiceStub) {
     let service = AuthServiceStub()
-    let sut = LogoutViewModel(service: service)
+    let sut = LogoutViewModel(service: service, authToken: authToken)
     
     trackForMemoryLeaks(sut, file: file, line: line)
     
