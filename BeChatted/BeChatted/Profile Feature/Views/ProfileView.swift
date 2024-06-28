c325e8d0bd86abd5e5121d084e22f3b8fdfb10b2
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-06-28 08:24:41 +0300

    Implement ProfileView UI

--- BeChatted/BeChatted/Profile Feature/Views/ProfileView.swift
+++ BeChatted/BeChatted/Profile Feature/Views/ProfileView.swift
@@ -8,11 +8,74 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    VStack {
+      header
+      
+      Divider()
+            
+      List {
+        userInfo
+        logoutButton
+      }
+      .listStyle(.plain)
+      .padding(.top, -8)
     }
+  }
+}
+
+extension ProfileView {
+  private var header: some View {
+    VStack {
+      ZStack {
+        Text("Profile")
+          .font(.title2)
+          .fontWeight(.medium)
+        
+        HStack {
+          Button {
+          } label: {
+            Image(systemName: "xmark")
+              .foregroundStyle(.black)
+              .font(.system(size: 32, weight: .thin))
+          }
+          .padding(.horizontal)
+          Spacer()
+        }
+      }
+    }
+    .frame(height: 64)
+  }
+}
+
+extension ProfileView {
+  private var userInfo: some View {
+    HStack {
+      ImageProvider.avatarPrototype
+        .resizable()
+        .frame(width: 64, height: 64)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+      
+      VStack(alignment: .leading) {
+        Text("Luka Rask")
+          .font(.system(size: 28, weight: .bold))
+        Text(verbatim: "luka.rusk@example.com")
+          .font(.system(size: 16, weight: .thin))
+      }
+      .padding(.horizontal)
+    }
+    .listRowSeparator(.hidden)
+  }
+}
+
+extension ProfileView {
+  private var logoutButton: some View {
+    Button("Logout") {
+    }
+    .buttonStyle(SecondaryButtonStyle())
+  }
 }
 
 #Preview {
-    ProfileView()
+  ProfileView()
 }
