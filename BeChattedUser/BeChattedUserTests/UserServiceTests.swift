276ec5ec0755f2efec2d979655eec4b84718c8c3
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-07-05 08:50:37 +0300

    UserService.user(by:authTokeh) sends userByEmail request

--- BeChattedUser/BeChattedUserTests/UserServiceTests.swift
+++ BeChattedUser/BeChattedUserTests/UserServiceTests.swift
@@ -7,23 +7,79 @@
 
 import XCTest
 
-protocol HTTPClientProtocol {}
+public protocol HTTPClientProtocol {
+  func perform(
+    request: URLRequest,
+    completion: @escaping (Result<(Data?, HTTPURLResponse?), Error>) -> Void
+  )
+}
+
+public struct UserData {
+  public let id: String
+  public let name: String
+  public let email: String
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+    case name
+    case email
+  }
+}
+
+public enum UserServiceError: Error {
+  case server
+  case connectivity
+}
 
 public struct UserService {
-  let client: HTTPClientProtocol
+  private let url: URL
+  private let client: HTTPClientProtocol
+  
+  public init(url: URL, client: HTTPClientProtocol) {
+    self.url = url
+    self.client = client
+  }
+  
+  public func user(
+    by email: String,
+    authToken: String,
+    completion: (Result<UserData, UserServiceError>) -> Void
+  ) {
+    let request = URLRequest(url: url)
+    
+    client.perform(request: request) { _ in }
+  }
 }
 
 final class UserServiceTests: XCTestCase {
   func test_init_doesNotSendRequests() {
+    let url = URL(string: "http://any-url.com")!
     let client = HTTPClientSpy()
-    let _ = UserService(client: client)
+    let _ = UserService(url: url, client: client)
     
     XCTAssertEqual(client.userByEmailCallCount, 0)
   }
   
+  func test_userByEmail_sendsUserByEmailRequest() {
+    let url = URL(string: "http://any-url.com")!
+    let client = HTTPClientSpy()
+    let sut = UserService(url: url, client: client)
+    
+    sut.user(by: "mail@example.com", authToken: "token") { _ in }
+    
+    XCTAssertEqual(client.userByEmailCallCount, 1)
+  }
+  
   // MARK: - Helpers
   
-  private struct HTTPClientSpy: HTTPClientProtocol {
+  private class HTTPClientSpy: HTTPClientProtocol {
     private(set) var userByEmailCallCount = 0
+    
+    func perform(
+      request: URLRequest,
+      completion: @escaping (Result<(Data?, HTTPURLResponse?), any Error>) -> Void
+    ) {
+      userByEmailCallCount += 1
+    }
   }
 }
