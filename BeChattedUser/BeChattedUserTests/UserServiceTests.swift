d838a38696d6cd28128bb77ca635a2d4dd4b6490
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-07-06 09:14:19 +0300

    Add makeSUT helper method

--- BeChattedUser/BeChattedUserTests/UserServiceTests.swift
+++ BeChattedUser/BeChattedUserTests/UserServiceTests.swift
@@ -53,17 +53,13 @@ public struct UserService {
 
 final class UserServiceTests: XCTestCase {
   func test_init_doesNotSendRequests() {
-    let url = URL(string: "http://any-url.com")!
-    let client = HTTPClientSpy()
-    let _ = UserService(url: url, client: client)
+    let (_, client) = makeSUT()
     
     XCTAssertEqual(client.userByEmailCallCount, 0)
   }
   
   func test_userByEmail_sendsUserByEmailRequest() {
-    let url = URL(string: "http://any-url.com")!
-    let client = HTTPClientSpy()
-    let sut = UserService(url: url, client: client)
+    let (sut, client) = makeSUT()
     
     sut.user(by: "mail@example.com", authToken: "token") { _ in }
     
@@ -71,6 +67,34 @@ final class UserServiceTests: XCTestCase {
   }
   
   // MARK: - Helpers
+  
+  private func makeSUT(
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) -> (UserService, HTTPClientSpy) {
+    let url = URL(string: "http://any-url.com")!
+    let client = HTTPClientSpy()
+    let sut = UserService(url: url, client: client)
+    
+    trackForMemoryLeaks(client, file: file, line: line)
+    
+    return (sut, client)
+  }
+  
+  func trackForMemoryLeaks(
+    _ object: AnyObject,
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) {
+    addTeardownBlock { [weak object] in
+      XCTAssertNil(
+        object,
+        "Expected object to be nil. Potential memory leak",
+        file: file,
+        line: line
+      )
+    }
+  }
   
   private class HTTPClientSpy: HTTPClientProtocol {
     private(set) var userByEmailCallCount = 0
