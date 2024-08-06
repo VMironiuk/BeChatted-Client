6e1c23debdcd421d2fdc0857bc7702a4a8dc0ac3
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-08-06 08:30:20 +0300

    ChannelCreationService_WebSocket.addChannel sends emit message with event and items

--- BeChattedChannels/BeChattedChannelsTests/ChannelCreationServiceTests_WebSocket.swift
+++ BeChattedChannels/BeChattedChannelsTests/ChannelCreationServiceTests_WebSocket.swift
@@ -16,6 +16,10 @@ struct ChannelCreationService_WebSocket {
     
     webSocketClient.connect()
   }
+  
+  func addChannel(_ channel: CreateChanelPayload) {
+    webSocketClient.emit("newChannel", channel)
+  }
 }
 
 final class ChannelCreationServiceTests_WebSocket: XCTestCase {
@@ -34,13 +38,39 @@ final class ChannelCreationServiceTests_WebSocket: XCTestCase {
     XCTAssertEqual(socket.onCallCount, 0)
   }
   
+  func test_addChannel_sendsEmitMessageWithNewChannelPayloadWithCorrectEvent() {
+    // given
+    let url = URL(string: "http://any-url.com")!
+    let socket = WebSocketClientSpy(url: url)
+    let sut = ChannelCreationService_WebSocket(webSocketClient: socket)
+    let channel = CreateChanelPayload(name: "channel name", description: "channel description")
+    
+    // when
+    sut.addChannel(channel)
+    
+    // then
+    XCTAssertEqual(socket.connectCallCount, 1)
+    XCTAssertEqual(socket.disconnectCallCount, 0)
+    XCTAssertEqual(socket.emitCallCount, 1)
+    XCTAssertEqual(socket.onCallCount, 0)
+    
+    XCTAssertEqual(socket.emitMessages[0].event, "newChannel")
+    XCTAssertEqual(socket.emitMessages[0].items[0] as! CreateChanelPayload, channel)
+  }
+  
   // MARK: - Helpers
   
   private final class WebSocketClientSpy: WebSocketClientProtocol {
+    struct EmitMessage {
+      let event: String
+      let items: [Any]
+    }
+    
     private(set) var connectCallCount = 0
     private(set) var disconnectCallCount = 0
-    private(set) var emitCallCount = 0
+    var emitCallCount: Int { emitMessages.count }
     private(set) var onCallCount = 0
+    private(set) var emitMessages = [EmitMessage]()
     
     init(url: URL) {
     }
@@ -54,7 +84,7 @@ final class ChannelCreationServiceTests_WebSocket: XCTestCase {
     }
     
     func emit(_ event: String, _ items: Any...) {
-      emitCallCount += 1
+      emitMessages.append(EmitMessage(event: event, items: items))
     }
     
     func on(_ event: String, completion: @escaping ([Any]) -> Void) {
