96004c80c0021c116dbf9e11f4d20927124dfcd4
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-08-22 08:17:20 +0300

    Add MessagingServiceTests

---
+++ BeChattedMessaging/BeChattedMessagingTests/MessagingServiceTests.swift
@@ -0,0 +1,11 @@
+//
+//  MessagingServiceTests.swift
+//  BeChattedMessagingTests
+//
+//  Created by Volodymyr Myroniuk on 22.08.2024.
+//
+
+import XCTest
+
+final class MessagingServiceTests: XCTestCase {
+}
