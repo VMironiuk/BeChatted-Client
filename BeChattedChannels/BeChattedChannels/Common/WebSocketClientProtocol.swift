c28332603a808b7d1651af2f703a5ed2cc172b0b
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-08-22 08:28:37 +0300

    Assert that MessagingService.init calls web socket's 'connect'

--- BeChattedChannels/BeChattedChannels/Common/WebSocketClientProtocol.swift
+++ BeChattedChannels/BeChattedChannels/Common/WebSocketClientProtocol.swift
@@ -11,8 +11,6 @@ public protocol WebSocketClientProtocol {
   init(url: URL)
   func connect()
   func disconnect()
-  // Because of shitty implementation on server side, which
-  // cannot handle arrays this piece of shit for emitting is mandatory
   func emit(_ event: String, _ item1: String, _ item2: String)
   func on(_ event: String, completion: @escaping ([Any]) -> Void)
 }
--- BeChattedMessaging/BeChattedMessagingTests/MessagingServiceTests.swift
+++ BeChattedMessaging/BeChattedMessagingTests/MessagingServiceTests.swift
@@ -7,5 +7,35 @@
 
 import XCTest
 
+protocol WebSocketClientProtocol {
+  func connect()
+}
+
+struct MessagingService {
+  private let webSocketClient: WebSocketClientProtocol
+  
+  init(webSocketClient: WebSocketClientProtocol) {
+    self.webSocketClient = webSocketClient
+    
+    webSocketClient.connect()
+  }
+}
+
 final class MessagingServiceTests: XCTestCase {
+  func test_init_setsUpWebSocketClient() {
+    let webSocketClient = WebSocketClientSpy()
+    _ = MessagingService(webSocketClient: webSocketClient)
+    
+    XCTAssertEqual(webSocketClient.connectCallCount, 1)
+  }
+  
+  // MARK: - Helpers
+  
+  private final class WebSocketClientSpy: WebSocketClientProtocol {
+    private(set) var connectCallCount = 0
+    
+    func connect() {
+      connectCallCount += 1
+    }
+  }
 }
