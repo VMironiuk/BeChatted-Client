b091998b5cdba6dd51bb8066c01c66d893054721
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-08-23 08:40:58 +0300

    Assert that MessagingService.sendMessage emits "newMessage" event

--- BeChattedMessaging/BeChattedMessagingTests/MessagingServiceTests.swift
+++ BeChattedMessaging/BeChattedMessagingTests/MessagingServiceTests.swift
@@ -10,9 +10,27 @@ import XCTest
 protocol WebSocketClientProtocol {
   func connect()
   func on(_ event: String, completion: @escaping ([Any]) -> Void)
+  func emit(
+    _ event: String,
+    _ item1: String,
+    _ item2: String,
+    _ item3: String,
+    _ item4: String,
+    _ item5: String,
+    _ item6: String
+  )
 }
 
 struct MessagingService {
+  struct Message {
+    let body: String
+    let userID: String
+    let channelID: String
+    let userName: String
+    let userAvatar: String
+    let userAvatarColor: String
+  }
+  
   private let webSocketClient: WebSocketClientProtocol
   
   init(webSocketClient: WebSocketClientProtocol) {
@@ -21,6 +39,18 @@ struct MessagingService {
     webSocketClient.connect()
     webSocketClient.on("messageCreated") { _ in }
     webSocketClient.on("userTypingUpdate") { _ in }
+  }
+  
+  func sendMessage(_ message: Message) {
+    webSocketClient.emit(
+      "newMessage",
+      message.body,
+      message.userID,
+      message.channelID,
+      message.userName,
+      message.userAvatar,
+      message.userAvatarColor
+    )
   }
 }
 
@@ -34,6 +64,30 @@ final class MessagingServiceTests: XCTestCase {
     XCTAssertTrue(webSocketClient.onMessages.map { $0.event }.contains("userTypingUpdate"))
   }
   
+  func test_sendMessage_emitsNewMessage() {
+    let message = MessagingService.Message(
+      body: "message body",
+      userID: "007",
+      channelID: "777",
+      userName: "James",
+      userAvatar: "user avatar",
+      userAvatarColor: "user avatar color"
+    )
+    let webSocketClient = WebSocketClientSpy()
+    let sut = MessagingService(webSocketClient: webSocketClient)
+    
+    sut.sendMessage(message)
+    
+    XCTAssertEqual(webSocketClient.emitCallCount, 1)
+    XCTAssertEqual(webSocketClient.emitMessages[0].event, "newMessage")
+    XCTAssertEqual(webSocketClient.emitMessages[0].item1, message.body)
+    XCTAssertEqual(webSocketClient.emitMessages[0].item2, message.userID)
+    XCTAssertEqual(webSocketClient.emitMessages[0].item3, message.channelID)
+    XCTAssertEqual(webSocketClient.emitMessages[0].item4, message.userName)
+    XCTAssertEqual(webSocketClient.emitMessages[0].item5, message.userAvatar)
+    XCTAssertEqual(webSocketClient.emitMessages[0].item6, message.userAvatarColor)
+  }
+  
   // MARK: - Helpers
   
   private final class WebSocketClientSpy: WebSocketClientProtocol {
@@ -42,8 +96,22 @@ final class MessagingServiceTests: XCTestCase {
       let completion: ([Any]) -> Void
     }
     
+    struct EmitMessage {
+      let event: String
+      let item1: String
+      let item2: String
+      let item3: String
+      let item4: String
+      let item5: String
+      let item6: String
+    }
+    
     private(set) var onMessages = [OnMessage]()
+    private(set) var emitMessages = [EmitMessage]()
     private(set) var connectCallCount = 0
+    var emitCallCount: Int {
+      emitMessages.count
+    }
     
     func connect() {
       connectCallCount += 1
@@ -51,6 +119,28 @@ final class MessagingServiceTests: XCTestCase {
     
     func on(_ event: String, completion: @escaping ([Any]) -> Void) {
       onMessages.append(OnMessage(event: event, completion: completion))
+    }
+    
+    func emit(
+      _ event: String,
+      _ item1: String,
+      _ item2: String,
+      _ item3: String,
+      _ item4: String,
+      _ item5: String,
+      _ item6: String
+    ) {
+      emitMessages.append(
+        EmitMessage(
+          event: event,
+          item1: item1,
+          item2: item2,
+          item3: item3,
+          item4: item4,
+          item5: item5,
+          item6: item6
+        )
+      )
     }
   }
 }
