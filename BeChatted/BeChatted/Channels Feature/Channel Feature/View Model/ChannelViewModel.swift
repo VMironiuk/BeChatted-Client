37ef954816456bcd32bd46eda4f65c4f48758563
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-09-05 09:17:48 +0300

    Add ChannelViewModel class as a starting point for the messaging feature

---
+++ BeChatted/BeChatted/Channels Feature/Channel Feature/View Model/ChannelViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ChannelViewModel.swift
+//  BeChatted
+//
+//  Created by Volodymyr Myroniuk on 05.09.2024.
+//
+
+import Foundation
+
+final class ChannelViewModel: ObservableObject {}
---
+++ BeChatted/BeChattedTests/ChannelViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  ChannelViewModelTests.swift
+//  BeChattedTests
+//
+//  Created by Volodymyr Myroniuk on 05.09.2024.
+//
+
+import Foundation
